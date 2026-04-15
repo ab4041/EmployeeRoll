5eeaaa117bcf9587db1811379dcf505efd63e1f7
ab4041 <138356134+ab4041@users.noreply.github.com>
2026-04-15 22:16:35 +0530

    UC5: Implement logging using Lombok @Slf4j

--- employeeroll/src/main/java/com/bridgelabz/employeeroll/controller/EmployeePayrollController.java
+++ employeeroll/src/main/java/com/bridgelabz/employeeroll/controller/EmployeePayrollController.java
@@ -4,11 +4,14 @@ import com.bridgelabz.employeeroll.dto.EmployeePayrollDTO;
 import com.bridgelabz.employeeroll.model.EmployeePayrollData;
 import com.bridgelabz.employeeroll.service.IEmployeePayrollService;
 
+import lombok.extern.slf4j.Slf4j;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
+@Slf4j
 @RestController
 @RequestMapping("/employeepayrollservice")
 public class EmployeePayrollController {
@@ -16,46 +19,64 @@ public class EmployeePayrollController {
     @Autowired
     private IEmployeePayrollService employeePayrollService;
 
+
     @GetMapping("/")
     public String getMessage() {
+
+        log.info("Received request for welcome message");
 
         return "Welcome to Employee Payroll App";
     }
 
+
     @GetMapping("/get")
     public List<EmployeePayrollData> getEmployeePayrollData() {
 
+        log.info("Fetching all employee payroll data");
+
         return employeePayrollService.getEmployeePayrollData();
     }
+
 
     @GetMapping("/get/{empId}")
     public EmployeePayrollData getEmployeePayrollDataById(
             @PathVariable int empId) {
 
+        log.info("Fetching employee data for ID: {}", empId);
+
         return employeePayrollService
                 .getEmployeePayrollDataById(empId);
     }
+
 
     @PostMapping("/create")
     public EmployeePayrollData createEmployeePayrollData(
             @RequestBody EmployeePayrollDTO dto) {
 
+        log.info("Creating employee: {}", dto);
+
         return employeePayrollService
                 .createEmployeePayrollData(dto);
     }
+
 
     @PutMapping("/update/{empId}")
     public EmployeePayrollData updateEmployeePayrollData(
             @PathVariable int empId,
             @RequestBody EmployeePayrollDTO dto) {
 
+        log.info("Updating employee ID: {}", empId);
+
         return employeePayrollService
                 .updateEmployeePayrollData(empId, dto);
     }
 
+
     @DeleteMapping("/delete/{empId}")
     public void deleteEmployeePayrollData(
             @PathVariable int empId) {
+
+        log.info("Deleting employee ID: {}", empId);
 
         employeePayrollService
                 .deleteEmployeePayrollData(empId);
